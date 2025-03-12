244137b3b52d7a84df18f6d1a2382ed38524ba0e
hladd375 <92380533+hladd375@users.noreply.github.com>
2025-03-12 09:05:14 -0400

    in class work 3/12/24

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -7,6 +7,7 @@ public class MyPlayer {
     public MyPlayer() {
         columns = new int[10];
 
+
         /***
          * This code will run just once, when the game opens.
          * Add your code here.
@@ -18,6 +19,7 @@ public class MyPlayer {
         System.out.println("MyPlayer Move");
 
         gameBoard = pBoard;
+        toColums();
         int column = 0;
         int row = 0;
 
@@ -32,6 +34,30 @@ public class MyPlayer {
 
         Point myMove = new Point(row, column);
         return myMove;
+    }
+
+    //todo: make a method called toCollums that converts gameboard to colums
+    public void toColums(){
+        int chipsInColum = 0;
+        for(int row = 0; row < gameBoard[0].length; row++){
+            for(int col = 0; col < gameBoard.length; col++){
+                if (gameBoard[col][row].isAlive == true) {
+                    chipsInColum ++;
+                }
+
+            }
+            System.out.println(chipsInColum);
+            for(int z = 0; z < columns.length; z++) {
+                columns[z] = chipsInColum;
+            }
+            chipsInColum = 0;
+        }
+        System.out.println("PRINTING FROM ARRAY");
+        for(int z = 0; z < columns.length; z++) {
+            System.out.println(columns[z]);
+        }
+
+
     }
 
 }
